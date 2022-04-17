fa2e5456aa36dd3d0969b6272d3f4c6ce0f0ba07
Rakshit Shrestha <rakxit121@outlook.com>
2022-04-17 18:00:27 +0545

    assignment

---
+++ Assignment2/src/Q.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+//Assignment 2
+public class Q {
+    public static void main(String[] args) {
+
+    }
+static class Q1 {
+    public static void main(String[] args) {
+
+//        1. Write a program to take student details as input and display the result.
+        // user input string
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first name: ");
+        String a = scanner.nextLine();
+        System.out.println("Enter last name: ");
+        String l = scanner.nextLine();
+        System.out.println("Enter class: ");
+        String cl = scanner.nextLine();
+        System.out.println("Enter age: ");
+        String b = scanner.nextLine();
+        System.out.println("Enter address: ");
+        String c = scanner.nextLine();
+
+        System.out.println("------Student details-----");
+        System.out.print("NAME: " + a);
+        System.out.println(" " + l);
+        System.out.println("CLASS: "+cl);
+        System.out.println("AGE: " + b);
+        System.out.println("ADDRESS: " + c);
+
+    }
+    }
+
+
+static class Q2 {
+        public static void main(String[] args) {
+
+//            2. Write a program to calculate sum of four numbers taking user input.
+
+        }
+    }
+}
