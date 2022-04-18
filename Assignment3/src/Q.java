3a44fc6e8d0ed9db68982a8ac1fe675faf3f0eee
Rakxit121 <rakxit121@gmail.com>
2022-04-18 19:54:54 +0545

    till8

--- Assignment3/src/Q.java
+++ Assignment3/src/Q.java
@@ -16,17 +16,17 @@ class Q1 {
         String fname = JOptionPane.showInputDialog("Enter your first name: ");
         String lname = JOptionPane.showInputDialog("Enter your last name: ");
         String rollno = JOptionPane.showInputDialog("Enter your Rollno.: ");
-        String age = JOptionPane.showInputDialog("Enter your age: ");
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age: "));
         String address = JOptionPane.showInputDialog("Enter your address: ");
         String batch = JOptionPane.showInputDialog("Enter your batch: ");
 
         JOptionPane.showMessageDialog(null,"----- User Data ----- \n Name:"+fname+" "+lname+"\nBatch: "+batch+"\nRollno. "+rollno+"\nAddress: "+address);
-        System.out.println("------Student details-----");
+        JOptionPane.showMessageDialog(null,"------Student details-----");
         System.out.print("NAME: " + fname);
-        System.out.println(" " + lname);
-        System.out.println("Batch: " + batch);
-        System.out.println("AGE: " + age);
-        System.out.println("ADDRESS: " + address);
+        JOptionPane.showMessageDialog(null," " + lname);
+        JOptionPane.showMessageDialog(null,"Batch: " + batch);
+        JOptionPane.showMessageDialog(null,"AGE: " + age);
+        JOptionPane.showMessageDialog(null,"ADDRESS: " + address);
 
     }
 }
@@ -40,7 +40,7 @@ class Q2 {
         // input the number
         double num1, num2, num3, num4;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter four number: ");
+        JOptionPane.showMessageDialog(null,"Enter four number: ");
 
         num1 = Double.parseDouble(JOptionPane.showInputDialog(null,"1st num"));
         num2 = Double.parseDouble(JOptionPane.showInputDialog(null,"2nd num"));
@@ -69,7 +69,7 @@ class Q3 {
             double eng, math, sci, com;
             Scanner op=new Scanner(System.in);
             /* Input marks of all five subjects */
-            System.out.println("Enter marks of four subjects:");
+            JOptionPane.showMessageDialog(null,"Enter marks of four subjects:");
             eng = Double.parseDouble(JOptionPane.showInputDialog("Enter marks of English subjects:"));
             math = Double.parseDouble(JOptionPane.showInputDialog("Enter marks of Maths subjects:"));
             sci = Double.parseDouble(JOptionPane.showInputDialog("Enter marks of Science subjects:"));
@@ -80,7 +80,7 @@ class Q3 {
             int total =(int) ((eng + math + sci + com)/400*100);
             int percentage = total;
 
-//        System.out.println("The total marks obtained from the four subjects is: "+percentage+"%");
+//        JOptionPane.showMessageDialog(null,"The total marks obtained from the four subjects is: "+percentage+"%");
             JOptionPane.showMessageDialog(null,"The obtained marks is--\n"+percentage+"\u0025");
         }
 }
@@ -91,12 +91,13 @@ class Q4{
 
         // input two integer number
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter two integer: ");
-        int num1 = scanner.nextInt();
-        int num2 = scanner.nextInt();
 
-        int sum = num1 +num2;
-        System.out.println("The of two integer "+num1+" and "+num2+" = "+sum);
+        double num1 = Double.parseDouble(JOptionPane.showInputDialog(null,"Enter 1st num: "));
+        double num2 = Double.parseDouble(JOptionPane.showInputDialog(null,"Enter 2nd num: "));
+
+        double sum = num1 +num2;
+        JOptionPane.showMessageDialog(null,"The of two integer "+num1+" and "+num2+" = "+sum);
+
     }
 }
 
@@ -105,15 +106,12 @@ class Q5{
     public static void main(String[] args) {
         /*    5.   Ask user to give two double input for length and breadth of a rectangle and print area type cast to int.*/
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter length: ");
-        double len = scanner.nextDouble();
-        System.out.println("Enter breadth: ");
-        double bdth = scanner.nextDouble();
+        Double length = Double.parseDouble(JOptionPane.showInputDialog(null,"Enter length of rectangle: "));
+        Double breadth = Double.parseDouble(JOptionPane.showInputDialog(null,"Enter length of rectangle: "));
 
         // output
-        int area = (int)len * (int)bdth;
-        System.out.println("The are is: "+ area);
+        Double area = length * breadth;
+        JOptionPane.showMessageDialog(null,"The are of reactangle is: "+area);
     }
 }
 
@@ -124,16 +122,14 @@ class Q6{
 //                my name is xyz and my roll number is xyz. My field of interest are xyz.
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("Enter your roll no. : ");
-        String rollno = scanner.nextLine();
-        System.out.println("Enter your interest : ");
-        String interest = scanner.nextLine();
+        String name = JOptionPane.showInputDialog(null, "Enter your name: ");
+        String interest = JOptionPane.showInputDialog(null, "Enter your interest: ");
+        int rollno = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter your Roll no.: "));
 
-        System.out.println("Hey,\n" +
+
+        JOptionPane.showMessageDialog(null,"Hey,\n" +
                 " Hey my name is"+ name +"and my roll number is "+rollno+". My field of interest are "+interest+".");
-
+        JOptionPane.showMessageDialog(null," Hey my name is"+ name +" and my roll number is "+rollno+". My field of interest are "+interest+"." );
     }
 }
 
@@ -146,35 +142,21 @@ class Q7{
         Scanner scanner = new Scanner(System.in);
 
         // input the length of a square
-        System.out.println("For area and volume of square");
-        System.out.println("Enter length of square: ");
-        double len = scanner.nextDouble();
+        double len = Double.parseDouble(JOptionPane.showInputDialog(null,"For area and volume of square\n"+"Enter the length of a square"));
 
-        int area = (int)len^2;
-        int volume = (int)len^3;
-
-        System.out.println();
+        int area = (int)(len * len);
+        int volume = (int)(len*len*len);
+        int volume_cube = area*area*area;
         // output
-        System.out.println("The area  of square is : "+area);
-        System.out.println("The volume  of square is : "+volume);
+        JOptionPane.showMessageDialog(null,"The input length of square is: "+len+"\n"+"The area  of square is : "+area+"\n"+"The volume  of square is : "+volume+"\n"+
+                "Using the same length of square- "+len+" as length of cube.\n"+"The volume of cube is: "+volume_cube);
 
-        System.out.println("Using the same length of square- "+len+" as length of cube");
-        int volume_cube = area^3;
-
-        System.out.println("The volume of cube is: "+volume_cube);
-
-        System.out.println();
-
-        System.out.println("For area of triangle");
-        System.out.println("Enter length of triangle: ");
-        double len_tri = scanner.nextDouble();
-        System.out.println("Enter breadth of triangle: ");
-        double height_tri = scanner.nextDouble();
-
-        double area_tri = len_tri*height_tri;
+        double len_tri = Double.parseDouble(JOptionPane.showInputDialog(null,"For area of triangle: "+"Enter the length of a triangle: "));
+        double height_tri = Double.parseDouble(JOptionPane.showInputDialog(null,"Enter the length of a triangle: "));
+        double area_tri = (len_tri*height_tri)/2;
 
         // output
-        System.out.println("The area of triangle is : "+(area_tri/2));
+        JOptionPane.showMessageDialog(null,"The area of triangle is : "+area_tri);
 
     }
 }
@@ -189,14 +171,14 @@ class Q8{
         Scanner scanner = new Scanner(System.in);
 
         // input number to square
-        System.out.println("Enter the number you want to square square: ");
+        JOptionPane.showMessageDialog(null,"Enter the number you want to square square: ");
         double num = scanner.nextDouble();
 
         int square = (int)num * (int)num;
         int cube = (int)num * (int)num *(int)num;
 
-        System.out.println("The square of input number is: "+square);
-        System.out.println("The cube of input number is: "+cube);
+        JOptionPane.showMessageDialog(null,"The square of input number is: "+square);
+        JOptionPane.showMessageDialog(null,"The cube of input number is: "+cube);
     }
 }
 
@@ -209,14 +191,14 @@ class Q9{
 //        OUTPUT : CodesDope
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter string: ");
+        JOptionPane.showMessageDialog(null,"Enter string: ");
         String str1 = scanner.nextLine();
-        System.out.println("Enter second string: ");
+        JOptionPane.showMessageDialog(null,"Enter second string: ");
         String str2 = scanner.nextLine();
 
-        System.out.println("------String-----");
+        JOptionPane.showMessageDialog(null,"------String-----");
         System.out.print(str1);
-        System.out.println(str2);
+        JOptionPane.showMessageDialog(null,str2);
     }
 }
 
@@ -229,17 +211,17 @@ class Q10{
 //                ( use && || with ternary operator )
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter three numbers: ");
+        JOptionPane.showMessageDialog(null,"Enter three numbers: ");
         int num1 = scanner.nextInt();
         int num2 = scanner.nextInt();
         int num3 = scanner.nextInt();
 
         if ((num1 == num2) && (num1== num2) && (num2==num3)) {
-            System.out.println("All are equal");
+            JOptionPane.showMessageDialog(null,"All are equal");
         }else if ((num1 == num2) || (num1== num2) || (num2==num3)) {
-            System.out.println("2 numbers are equal");
+            JOptionPane.showMessageDialog(null,"2 numbers are equal");
         }else {
-            System.out.println("Numbers are not  equal");
+            JOptionPane.showMessageDialog(null,"Numbers are not  equal");
         }
     }
 }
@@ -249,15 +231,15 @@ class Q11{
 //        check if both the conditions 'a < 50' and 'a < b' are true.
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter value for a: ");
+        JOptionPane.showMessageDialog(null,"Enter value for a: ");
         int a = scanner.nextInt();
-        System.out.println("Enter value for b: ");
+        JOptionPane.showMessageDialog(null,"Enter value for b: ");
         int b = scanner.nextInt();
 
         if ((a<50) && (a<b)){
-            System.out.println("The condition a<50 and a<b are true");
+            JOptionPane.showMessageDialog(null,"The condition a<50 and a<b are true");
         }else {
-            System.out.println("The condition a<50 and a<b are false");
+            JOptionPane.showMessageDialog(null,"The condition a<50 and a<b are false");
         }
     }
 }
@@ -271,7 +253,7 @@ class Q12{
         double total,percentage;
         Scanner op=new Scanner(System.in);
         /* Input marks of all five subjects */
-        System.out.println("Enter three marks of Robert:");
+        JOptionPane.showMessageDialog(null,"Enter three marks of Robert:");
         System.out.print("Enter marks of English:");
         eng=op.nextFloat();
         System.out.print("Enter marks of Maths:");
@@ -283,7 +265,7 @@ class Q12{
         total = eng + math+ com;
         percentage = (total / 300.0) * 100;
 
-        System.out.println("The total marks obtained Roberts in three subjects is: "+percentage+"%");
+        JOptionPane.showMessageDialog(null,"The total marks obtained Roberts in three subjects is: "+percentage+"%");
     }
 
 }
@@ -297,14 +279,14 @@ class Q14{
         double total,percentage;
         Scanner op=new Scanner(System.in);
         /* Input marks of all five subjects */
-        System.out.println("Enter three marks of Robert:");
+        JOptionPane.showMessageDialog(null,"Enter three marks of Robert:");
         System.out.print("Enter marks of English:");
 
         /* Calculate total, percentage */
 //        total = eng + math+ com;
 //        percentage = (total / 300.0) * 100;
 //
-//        System.out.println("The total marks obtained Roberts in three subjects is: "+percentage+"%");
+//        JOptionPane.showMessageDialog(null,"The total marks obtained Roberts in three subjects is: "+percentage+"%");
     }
 
 }
