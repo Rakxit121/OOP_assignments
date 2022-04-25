0de3fe9da4af3a96f17c4dc4b6f16b1e58b92d31
Rakxit121 <rakxit121@gmail.com>
2022-04-25 16:00:57 +0545

    switch_assign_update

--- java_Assignment/src/Assignment4.java
+++ java_Assignment/src/Assignment4.java
@@ -713,3 +713,45 @@ class If_else_Q18{
         }
     }
 }
+
+//19. Write a JAVA program to input marks of five subjects Physics, Chemistry, Biology,
+//        Mathematics and Computer. Calculate percentage and grade according to following:
+//        Percentage >= 90% : Grade A
+//        Percentage >= 80% : Grade B
+//        Percentage >= 70% : Grade C
+//        Percentage >= 60% : Grade D
+//        Percentage >= 40% : Grade E
+//        Percentage < 40% : Grade F
+class If_else_Q19{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
+
+//20. Write a JAVA program to input basic salary of an employee and calculate its Gross salary
+//        according to following:
+//        Basic Salary <= 10000 : HRA = 20%, DA = 80%
+//        Basic Salary <= 20000 : HRA = 25%, DA = 90%
+//        Basic Salary > 20000 : HRA = 30%, DA = 95%
+class If_else_Q20{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
+
+//21. Write a JAVA program to input electricity unit charges and calculate total electricity bill
+//        according to the given condition:
+//        For first 50 units Rs. 0.50/unit
+//        For next 100 units Rs. 0.75/unit
+//        For next 100 units Rs. 1.20/unit
+//        For unit above 250 Rs. 1.50/unit
+//        An additional surcharge of 20% is added to the bill
+class If_else_Q21{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
